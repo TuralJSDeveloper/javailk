c7061c0cdc0cfe416d8e027f9a58af15ca8f0a39
TuralJSDeveloper <tural.azizxanli92@gmail.com>
2022-05-12 21:30:41 +0400

    java sinaq repozitori

---
+++ javaHesabiLk/src/javahesabilk/JavaHesabiLk.java
@@ -0,0 +1,19 @@
+ 
+package javahesabilk;
+
+/**
+ *
+ * @author Tural
+ */
+public class JavaHesabiLk {
+
+    
+    public static void main(String[] args) {
+         topla(12, 13);//netice 25
+    }
+    
+    public static int topla(int a, int b){
+        return a+b;
+    }
+    
+}
